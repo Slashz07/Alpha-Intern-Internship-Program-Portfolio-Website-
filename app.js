5f059ec9e2a894a2140d993523a24857559db527
Slashz07 <sumitforsaken76248@gmail.com>
2024-03-31 18:02:15 +0530

    light-dark mode button created

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const btnBox=document.querySelectorAll('.controls')
 const secBtns = document.querySelectorAll('.control')
 const allSections = document.querySelectorAll('body')
 
+// for color change in menubar buttons-->
 function pageTransition() {
   for (i = 0; i < secBtns.length; i++){
     secBtns[i].addEventListener('click' ,function(){//'this' keyword does not exist in arrow function  
@@ -11,7 +12,7 @@ function pageTransition() {
       this.classList.add('active-button');
     })
   }
-  
+  // for transitiions in between pages-->
   allSections[0].addEventListener('click', (e) => {
     const id = e.target.dataset.id;
     if (id) {
@@ -21,8 +22,16 @@ function pageTransition() {
       console.log(element);
       element.classList.add('active')
     }
+  })
 
-})
+  // themebutton-->
+  const themeBtn = document.querySelector(".theme-btn")
+  const element = document.body;
+  themeBtn.addEventListener('click', () => {
+  element.classList.toggle("light-mode")
+  })
+  
 }
 
 pageTransition()
+
